b5c210ec348ea7cc112e7ce893db168dec2f0dec
Keller18306 <kompeter17@gmail.com>
2024-12-31 17:00:25 +0300

    leak.js update

--- tests/leak.js
+++ tests/leak.js
@@ -1,7 +1,18 @@
-const mdbx = require('../build/Debug/node-mdbx.node');
+const mdbx = require('../index');
 
-console.log('Aaaa')
-const env = new mdbx.Env('/newhdd/mdbx/0-main/');
+mdbx.setup_debug({
+    logger: false
+})
+
+console.log('start')
+const env = new mdbx.Env({
+    path: 'db/',
+    geometry: {
+        sizeNow: 4 * 1024 * 1024 * 1024,
+        sizeLower: 4 * 1024 * 1024 * 1024,
+        sizeUpper: 4 * 1024 * 1024 * 1024
+    }
+});
 
 function logMemoryUsage(label) {
     const mem = process.memoryUsage();
@@ -20,41 +31,71 @@ function getRandomKey() {
     ).join('');
 }
 
-const dbi = env.getDbi('email');
-const test = [];
 
-function testCursor(ic) {
-    const cursor = dbi.getCursor();
 
-    const iterations = 1000000;
-    logMemoryUsage("Before test");
+const dbi = env.getDbi(null, {
+    create: true,
+    dupSort: true
+});
+
+function testCursorWrite1(ic) {
+    const txn = env.getTxn({});
+    const cursor = dbi.getCursor({ txn });
+
+    logMemoryUsage("Before test - write1");
+
+    const iterations = 30_000;
 
     for (let i = 0; i < iterations; i++) {
-        // const randomKey = getRandomKey();
-        const randomKey = 'test'
-        let value = cursor.range(Buffer.from(randomKey));
-        test.push(value);
-        // const [a, b] = cursor.next();
+        const randomKeyA = getRandomKey();
+        const randomKeyB = getRandomKey();
 
-        for (let i = 0; i <= 1000; i++) {
-            value = cursor.next();
-            test.push(value);
+        cursor.put(Buffer.from(randomKeyA), Buffer.from(randomKeyB));
 
-            if (i % 100 === 0) {
-                console.clear();
-                console.log(`Iteration ${ic}-${i}:`, value?.[0]?.toString(), value?.[1]?.toString());
-                logMemoryUsage("While iteration");
-            }
-        }
-
-        if (i % 10000 === 0) {
-            console.clear();
-            console.log(`Iteration ${ic}-${i}:`, value?.[0]?.toString(), value?.[1]?.toString());
-            logMemoryUsage("While iteration");
+        if (i % 1_000 === 0) {
+            // console.clear();
+            console.log(`Iteration ${ic}-${i}:`, randomKeyA, randomKeyB);
+            logMemoryUsage("While iteration - write1");
         }
     }
 
-    logMemoryUsage("After test");
+    logMemoryUsage("After test - write1");
+
+    cursor.close();
+    // console.log(txn.commitWithLatency());
+    txn.commit();
+
+    if (global.gc) {
+        global.gc();
+        logMemoryUsage("After GC");
+    } else {
+        console.warn("Сборщик мусора недоступен. Запустите с флагом '--expose-gc'.");
+    }
+
+    // debugger;
+}
+
+function testCursorWrite2(ic) {
+    const cursor = dbi.getCursor({});
+
+    logMemoryUsage("Before test - write2");
+
+    const iterations = 30_000;
+
+    for (let i = 0; i < iterations; i++) {
+        const randomKeyA = getRandomKey();
+        const randomKeyB = getRandomKey();
+
+        cursor.put(Buffer.from(randomKeyA), Buffer.from(randomKeyB));
+
+        if (i % 1_000 === 0) {
+            // console.clear();
+            console.log(`Iteration ${ic}-${i}:`, randomKeyA, randomKeyB);
+            logMemoryUsage("While iteration - write2");
+        }
+    }
+
+    logMemoryUsage("After test - write2");
 
     cursor.close();
 
@@ -68,6 +109,134 @@ function testCursor(ic) {
     // debugger;
 }
 
+function testCursorRead(ic) {
+    const cursor = dbi.getCursor({});
+
+    logMemoryUsage("Before test - read");
+
+    const iterations = 10_000;
+
+    for (let i = 0; i < iterations; i++) {
+        const randomKey = getRandomKey();
+
+        cursor.range(Buffer.from(randomKey));
+        // const [a, b] = cursor.next();
+
+        for (let j = 0; j <= 10; j++) {
+            cursor.next();
+
+            // if (j % 100 === 0) {
+            //     console.clear();
+            //     console.log(`Iteration ${ic}-${i}-${j}:`, value?.[0]?.toString(), value?.[1]?.toString());
+            //     logMemoryUsage("While iteration (read)");
+            // }
+        }
+
+        if (i % 1_000 === 0) {
+            // console.clear();
+            console.log(`Iteration ${ic}-${i}:`, cursor.getKey()?.toString(), cursor.getValue()?.toString());
+            logMemoryUsage("While iteration - read");
+        }
+    }
+
+    logMemoryUsage("After test - read");
+
+    cursor.close();
+
+    if (global.gc) {
+        global.gc();
+        logMemoryUsage("After GC - read");
+    } else {
+        console.warn("Сборщик мусора недоступен. Запустите с флагом '--expose-gc'.");
+    }
+
+    // debugger;
+}
+
+function testTxnWriteAbort(ic) {
+    const txn = env.getTxn({});
+    const cursor = dbi.getCursor({ txn });
+
+    logMemoryUsage("Before test - testTxnWriteAbort");
+
+    for (let i = 0; i < 10; i++) {
+        const randomKeyA = getRandomKey();
+        const randomKeyB = getRandomKey();
+
+        for (let j = 0; j < 1000; j++) {
+            cursor.put(Buffer.from(randomKeyA), Buffer.from(randomKeyB));
+        }
+
+        console.log(`Iteration ${ic}-${i}:`, randomKeyA, randomKeyB);
+        logMemoryUsage("While iteration - testTxnWriteAbort");
+    }
+
+    logMemoryUsage("After test - write1");
+
+    cursor.close();
+    txn.abort()
+
+    if (global.gc) {
+        global.gc();
+        logMemoryUsage("After GC");
+    } else {
+        console.warn("Сборщик мусора недоступен. Запустите с флагом '--expose-gc'.");
+    }
+}
+
+function testTxnRead(ic) {
+    const txn = env.getTxn({ readOnly: true });
+    const cursor = dbi.getCursor({ txn });
+
+    logMemoryUsage("Before test - testTxnWriteAbort");
+
+    for (let i = 0; i < 5; i++) {
+        const randomKeyA = getRandomKey();
+
+        cursor.range(Buffer.from(randomKeyA));
+
+        for (let j = 0; j < 1000; j++) {
+            cursor.next();
+        }
+
+        console.log(`Iteration ${ic}-${i}:`, randomKeyA);
+        logMemoryUsage("While iteration - testTxnWriteAbort");
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const randomKeyA = getRandomKey();
+        const randomKeyB = getRandomKey();
+
+        const val = cursor.range(Buffer.from(randomKeyA), Buffer.from(randomKeyB));
+        if (!val) {
+            continue;
+        }
+
+        for (let j = 0; j < 1000; j++) {
+            cursor.next();
+        }
+
+        console.log(`Iteration ${ic}-${i}:`, randomKeyA, randomKeyB);
+        logMemoryUsage("While iteration - testTxnWriteAbort");
+    }
+
+    logMemoryUsage("After test - write1");
+
+    cursor.close();
+    txn.commit();
+
+    if (global.gc) {
+        global.gc();
+        logMemoryUsage("After GC");
+    } else {
+        console.warn("Сборщик мусора недоступен. Запустите с флагом '--expose-gc'.");
+    }
+}
+
 for (let i = 0; i < 10; i++) {
-    testCursor(i);
-} 
+    testCursorWrite1(i);
+    testCursorWrite2(i);
+    testCursorRead(i);
+    testTxnWriteAbort(i)
+    testTxnRead(i);
+}

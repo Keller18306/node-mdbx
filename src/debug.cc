b623b521cbfe909c9d28c640a473cba05453e911
Keller18306 <kompeter17@gmail.com>
2024-12-31 16:55:41 +0300

    debug logger

--- src/debug.cc
+++ src/debug.cc
@@ -2,12 +2,30 @@
 
 #include "utils.h"
 
+const char* LogLevelToString(MDBX_log_level_t loglevel) {
+    switch (loglevel) {
+        case MDBX_LOG_FATAL: return "FATAL";
+        case MDBX_LOG_ERROR: return "ERROR";
+        case MDBX_LOG_WARN: return "WARN";
+        case MDBX_LOG_NOTICE: return "NOTICE";
+        case MDBX_LOG_VERBOSE: return "VERBOSE";
+        case MDBX_LOG_DEBUG: return "DEBUG";
+        case MDBX_LOG_TRACE: return "TRACE";
+        case MDBX_LOG_EXTRA: return "EXTRA";
+        case MDBX_LOG_DONTCHANGE: return "DONTCHANGE";
+        default: return "UNKNOWN";
+    }
+}
+
+
 void MDBX_Debug::Init(Napi::Env env, Napi::Object exports) {
 	exports.Set("setup_debug", Napi::Function::New<MDBX_Debug::Setup>(env));
 }
 
 void MDBX_Debug::Setup(const Napi::CallbackInfo &info) {
 	int flags = MDBX_DBG_NONE;
+	MDBX_log_level_t log_level = MDBX_LOG_DONTCHANGE;
+	MDBX_debug_func *logger = nullptr;
 
 	Napi::Object options = info[0].ToObject();
 
@@ -16,5 +34,17 @@ void MDBX_Debug::Setup(const Napi::CallbackInfo &info) {
 	Utils::setFromObject(&flags, MDBX_DBG_LEGACY_OVERLAP, options, "legacyOverlap");
 	Utils::setFromObject(&flags, MDBX_DBG_DONT_UPGRADE, options, "dontUpgrade");
 
-	mdbx_setup_debug(MDBX_log_level_t::MDBX_LOG_DONTCHANGE, static_cast<MDBX_debug_flags>(flags), nullptr);
+	if (options.Get("logger").IsBoolean() && options.Get("logger").ToBoolean().Value()) {
+        // Сохраняем JavaScript функцию в глобальной ссылке
+        logger = &Logger;
+		log_level = MDBX_LOG_EXTRA;
+    }
+
+	mdbx_setup_debug(log_level, static_cast<MDBX_debug_flags>(flags), &Logger);
+}
+
+void MDBX_Debug::Logger(MDBX_log_level_t loglevel, const char *function, int line, const char *fmt, va_list args) noexcept {
+    char message[1024];
+    vsnprintf(message, sizeof(message), fmt, args);
+	printf("[%s] %s():%d %s", LogLevelToString(loglevel), function, line, message);
 }
--- src/debug.h
+++ src/debug.h
@@ -4,6 +4,7 @@
 class MDBX_Debug : public Napi::ObjectWrap<MDBX_Debug> {
     private:
 	  static void Setup(const Napi::CallbackInfo &info);
+	  static void Logger(MDBX_log_level_t loglevel, const char *function, int line, const char *fmt, va_list args) noexcept;
 
 	public:
 	  static void Init(Napi::Env env, Napi::Object exports);
